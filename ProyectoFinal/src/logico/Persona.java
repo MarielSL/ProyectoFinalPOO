0c520e3bced830de880ada65fa05fce41b71bed6
Mariel Sanchez <mxsl0004@ce.pucmm.edu.do>
2026-07-15 23:14:20 -0400

    Creación de las clases a utilizar.

--- ProyectoFinal/src/logico/Persona.java
+++ ProyectoFinal/src/logico/Persona.java
@@ -1,6 +1,20 @@
 package logico;
 
-public class Persona {
+import java.time.LocalDate;
+
+public abstract class Persona {
 	private String id;
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	private LocalDate fechNacim;
+	private String telefono;
+	private String direccion;
+	private Sexo sexo;
+	private String ciudad;
+	private boolean dispParaMudarse;
+	private boolean licenciaConducir;
+	private boolean estadoEmpleo;
+	
 	
 }
---
+++ ProyectoFinal/src/logico/Sexo.java
@@ -0,0 +1,6 @@
+package logico;
+
+public enum Sexo {
+	FEMENINO,
+	MASCULINO
+}
